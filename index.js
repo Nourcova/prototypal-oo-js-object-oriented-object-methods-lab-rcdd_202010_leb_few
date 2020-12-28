0d350c519659d169daa67c86358dfd3898fb72db
Ibrahim Nour <ibrahamnour22@gmail.com>
2020-12-28 16:31:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function BoardMember(name, homeState, training){
     return "You can do that!";
   }
   BoardMember.prototype.doCharity=function(){
-    return "I like to help people";
+    return "I like to help people.";
+  }
+  BoardMember.prototype.releasePressStatment= function(){
+    return "You will see great things from Scuber."
+  }
+  BoardMember.prototype.sayHi=function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${training}`
   }
 }
